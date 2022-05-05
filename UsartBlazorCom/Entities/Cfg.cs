2d0ec8c660211ebc6a7497f51ea9947e9d96b59c
Symo <simon.hamanek@gmail.com>
2022-05-05 19:03:11 +0200

    major fixes saving added flashing added

---
+++ UsartBlazorCom/Entities/Cfg.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Ports;
+
+namespace UsartBlazorCom.Entities; 
+
+public class Cfg {
+    public string ProjectFolder { get; set; } = null!;
+    public string CurrentProjectName { get; set; } = null!;
+
+
+    public string PortName { get; set; } = null!;
+    public int BaudRate { get; set; }
+    public Parity Parity { get; set; }
+    public int DataBits { get; set; }
+    public StopBits StopBits { get; set; }
+    public Handshake Handshake { get; set; }
+}
